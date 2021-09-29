f3696837e9091b6f11ed19e118c7767e9e37e702
Onvyr <Onvyr@DESKTOP-Q3NIJVD>
2021-09-29 16:47:51 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
